e8a8cde9f16686b37b46af4569573a22dfdcf580
swapnilshinde123 <swapnil6901@gmail.com>
2021-01-26 10:54:58 +0530

    slack

--- slack/src/App.js
+++ slack/src/App.js
@@ -7,10 +7,9 @@ import "./App.css"
 function App() {
     return (
         <div className="app">
-              
             <Header />
             <div className="app_body">
-                <Sidebar/>
+                <Sidebar />
 
             </div>
 
